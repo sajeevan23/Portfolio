68025b113f2404147bc385beffe1fd817941a778
Sajeevan.B <sajeevanb788@gmail.com>
2022-05-12 23:21:20 +0530

    new changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,34 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== SERVICES MODAL ===============*/
+const modalView = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalClose = document.querySelectorAll('.services__modal-close')
 
+let modal = function(modalClick) {
+    modalView[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i) => {
+    mb.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalClose.forEach((mc) => {
+    mc.addEventListener('click', () => {
+        modalView.forEach((mv) =>   {
+            mv.classList.remove('active-modal')
+        })
+    })
+})
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
